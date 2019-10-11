309db8ef7b07c6eae78678a05977985f3da94949
erosgonzalez <egonzalez-lopez@csumb.edu>
2019-10-11 15:41:44 -0700

    update resume

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
         	<h5 style="font-size: 30px;">Eros Gonzalez-Lopez</h5><br/>
             <p style="font-size: 20px;">TEST
             <p>Some of my interest include: image processing, audio analysis, AI, music.</p>
-            <p><a><img src="images/Resume.png"></img></a></p>
+            <p><a><img src="images/Resume"></img></a></p>
         </div>
       </div>
     </div>
